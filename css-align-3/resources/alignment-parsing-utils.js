460935debae963c0a50c3225542443e8d678e77b
Javier Fernandez <jfernandez@igalia.com>
2017-03-08 17:22:09 +0100

    Added tests to verify the parsing of the place-self shorthand.

--- css-align-3/resources/alignment-parsing-utils.js
+++ css-align-3/resources/alignment-parsing-utils.js
@@ -1,19 +1,48 @@
+var selfPositionValues = [ "start", "end", "self-start", "self-end", "left", "right", "center", "flex-start", "flex-end"];
 var contentPositionValues = [ "start", "end", "left", "right", "center", "flex-start", "flex-end"];
 var distributionValues = [ "stretch", "space-around", "space-between", "space-evenly"];
 var baselineValues = [ "baseline", "first baseline", "last baseline"];
 
 function checkPlaceContent(alignValue, justifyValue = "")
 {
+    checkPlaceShorhand("place-content", "align-content", "justify-content", alignValue, justifyValue);
+}
+
+function checkPlaceSelf(alignValue, justifyValue = "")
+{
+    checkPlaceShorhand("place-self", "align-self", "justify-self", alignValue, justifyValue);
+}
+
+function checkPlaceShorhand(shorthand, alignLonghand, justifyLonghand, alignValue, justifyValue = "")
+{
     var div = document.createElement("div");
-    div.setAttribute("style", "place-content: " + alignValue + " " + justifyValue);
+    div.setAttribute("style", shorthand + ": " + alignValue + " " + justifyValue);
     document.body.appendChild(div);
     if (justifyValue === "")
         justifyValue = alignValue;
     var style = getComputedStyle(div);
-    assert_equals(style.getPropertyValue("place-content"),
-                  alignValue + " " + justifyValue, "place-content resolved value");
-    assert_equals(style.getPropertyValue("align-content"),
-                  alignValue, "align-content resolved value");
-    assert_equals(style.getPropertyValue("justify-content"),
-                  justifyValue, "justify-content resolved value");
+    assert_equals(style.getPropertyValue(shorthand),
+                  alignValue + " " + justifyValue, shorthand + " resolved value");
+    assert_equals(style.getPropertyValue(alignLonghand),
+                  alignValue, alignLonghand + " resolved value");
+    assert_equals(style.getPropertyValue(justifyLonghand),
+                  justifyValue, justifyLonghand + " resolved value");
+}
+
+function checkPlaceSelfInvalidValues(value)
+{
+    checkPlaceShorhandInvalidValues("place-self", "align-self", "justify-self", value);
+}
+
+function checkPlaceShorhandInvalidValues(shorthand, alignLonghand, justifyLonghand, value)
+{
+    var div = document.createElement("div");
+    var css = alignLonghand + ": start; " + justifyLonghand + ": end;" + shorthand + ": " + value;
+    div.setAttribute("style", css);
+    document.body.appendChild(div);
+    var style = getComputedStyle(div);
+    assert_equals(style.getPropertyValue(alignLonghand),
+                  "start", alignLonghand + " resolved value");
+    assert_equals(style.getPropertyValue(justifyLonghand),
+                  "end", justifyLonghand + " resolved value");
 }
